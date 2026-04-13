c32a92691c5171fe4fd2daf8292105db466b97cd
shevchenko <_shevchenko@mail.ru>
2026-04-13 22:16:43 +0300

    13-04-2026

--- app/src/main/java/com/example/merlioncoursenew/presentation/screens/FavoritesFragment.kt
+++ app/src/main/java/com/example/merlioncoursenew/presentation/screens/FavoritesFragment.kt
@@ -9,14 +9,6 @@ import com.example.merlioncoursenew.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoritesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavoritesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -25,8 +17,6 @@ class FavoritesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -52,8 +42,6 @@ class FavoritesFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             FavoritesFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
